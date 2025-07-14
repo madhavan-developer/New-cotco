786228914eba554d36748a2483294522074f44da
Foragefoxoffice <forgefoxoffice@gmail.com>
2025-07-14 10:28:48 +0530

    product page issues

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import Navbar from "../components/layout/Navbar";
-import Hero from "../components/products/hero";
+import Hero from "../components/coctoproducts/hero";
 import Footer from "../components/layout/Footer";
-import LmwMachines from "../components/products/LmwMachines";
+import LmwMachines from "../components/coctoproducts/LmwMachines";
 import ContactToday from "../components/common/ContactToday";
 const Products = () => {
   return (
