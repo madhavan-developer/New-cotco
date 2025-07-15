3f152fe77c60f9f6d971665eb42aed122204079d
Foragefoxoffice <forgefoxoffice@gmail.com>
2025-07-15 12:05:37 +0530

    font size change

--- src/components/coctoproducts/ProductsHero.jsx
+++ src/components/coctoproducts/ProductsHero.jsx
@@ -123,7 +123,7 @@ export default function ProductsHero() {
           variants={heroVariants}
         >
           <motion.h1
-            className="absolute z-11 bottom-10 left-8 text-center text-xl md:text-3xl font-semibold text-[#fff] mb-4 md:mb-6"
+            className="absolute z-11 bottom-10 left-8 text-center text-xl md:text-4xl font-semibold text-[#fff] mb-4 md:mb-6"
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.1 }}
